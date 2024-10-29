7ac56ee379cf380b8c7b944cf94458e836e007d3
sunqi <zqbysq@163.com>
2024-10-29 18:31:30 +0800

    update

--- src/util/svgTools.tsx
+++ src/util/svgTools.tsx
@@ -1,21 +1,34 @@
-export const getSVGForSrcById = ({ symbolId = "icon-symbol-one", color = 'red' }) => {
-      let svgstr = getSVGElementById({ symbolId, color })
-      let src = 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(svgstr)
+export const getSVGForSrcById = ({
+  symbolId = "icon-symbol-one",
+  color = "red",
+}: {
+  symbolId: string;
+  color: string;
+}) => {
+  let svgstr = getSVGElementById({ symbolId, color });
+  let src = "data:image/svg+xml;charset=utf-8," + encodeURIComponent(svgstr);
 
-      return src
-}
-export const getSVGElementById = ({ symbolId, color = 'red' }) => {
-      symbolId = symbolId || "icon-symbol-one"
-      var oSvg = document.querySelector(`symbol#${symbolId}`)
-      let paths = ""
-      if (oSvg) {
-            var pathRegex = /<path[^>]*>[^<]*<\/path>/gi
-            var pathMatches = oSvg.outerHTML.match(pathRegex);
-            if (pathMatches && pathMatches.length) {
-                  paths = pathMatches.join("")
-            }
-      }
+  return src;
+};
 
-      var svgStr = `<svg xmlns="http://www.w3.org/2000/svg" id="${symbolId}" viewBox="0 0 1024 1024" height="36px" width="36px" fill="${color}">${paths}</svg>`;
-      return svgStr
-}
+export const getSVGElementById = ({
+  symbolId,
+  color = "red",
+}: {
+  symbolId: string | undefined;
+  color: string;
+}) => {
+  symbolId = symbolId || "icon-symbol-one";
+  var oSvg = document.querySelector(`symbol#${symbolId}`);
+  let paths = "";
+  if (oSvg) {
+    var pathRegex = /<path[^>]*>[^<]*<\/path>/gi;
+    var pathMatches = oSvg.outerHTML.match(pathRegex);
+    if (pathMatches && pathMatches.length) {
+      paths = pathMatches.join("");
+    }
+  }
+
+  var svgStr = `<svg xmlns="http://www.w3.org/2000/svg" id="${symbolId}" viewBox="0 0 1024 1024" height="36px" width="36px" fill="${color}">${paths}</svg>`;
+  return svgStr;
+};
