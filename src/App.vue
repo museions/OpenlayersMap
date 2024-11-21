9a73994057f471d177b775abe2499e5308b9b1a8
sunqi <zqbysq@163.com>
2024-11-21 21:51:04 +0800

    update svg icons

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <script setup lang="ts">
+import { onBeforeMount } from "vue";
 import home from "./view/home.vue";
 import IndexModal from "./components/Modals/index.vue";
+
+const container: Element | null = document.querySelector('#svgBase')
+const fetchSymbols = () => {
+  fetch("./icons.svg").then(response => response.text()).then(content => {
+    if (container && content) {
+      container.innerHTML = content
+    }
+  })
+}
+
+onBeforeMount(() => {
+  fetchSymbols()
+})
 </script>
 
 <template>
